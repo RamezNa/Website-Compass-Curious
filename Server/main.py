b65d295852c6276ccb0ab9160da7dd4e6b883ca6
RamezNa <ramiznassar11@gmail.com>
2024-05-02 12:58:46 +0300

    make the server work with debug

--- Server/main.py
+++ Server/main.py
@@ -32,5 +32,6 @@ async def get_suggestion(location , day):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    # TODO remove the debug
+    uvicorn.run(app, host='0.0.0.0', port=8000, debug=True)
     # app.run(debug=True) 
