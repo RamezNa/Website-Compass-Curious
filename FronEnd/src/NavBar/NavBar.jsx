eb29dcc0f0790b4d35308b0e55406db4bdb83436
RamezNa <ramiznassar11@gmail.com>
2024-05-09 21:24:41 +0300

    build the nav bar

---
+++ FronEnd/src/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+// import images
+import logo_img from './Images/Logo.jpeg'
+import user_offline from './Images/User_Offline.png'
+
+
+// make a Navbar Html
+function NavBar(){
+    return <>
+    <nav className="nav_bar">
+        
+        <ul className='container_nav_bar'>
+            <li>
+                <img src={logo_img} alt="Logo" />
+            </li>
+            <li>
+                <a className="href_text" >Main</a>
+            </li>
+            <li>
+                <a className="href_text" >Trend</a>
+            </li>
+            <li>
+                <img src={user_offline} alt="Login" />
+            </li>
+        </ul>
+        
+    </nav>
+
+    </>
+}
+    
+export default NavBar
