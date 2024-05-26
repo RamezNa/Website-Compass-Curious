d847a6f5f127a0ebe25ac2655c3f19775ec649c7
RamezNa <ramiznassar11@gmail.com>
2024-05-26 22:06:23 +0300

    add the loading gif to img

--- FronEnd/src/Suggestion/component/SuggestCard.jsx
+++ FronEnd/src/Suggestion/component/SuggestCard.jsx
@@ -1,6 +1,9 @@
 import { Link, Element } from 'react-scroll';
+import { useState } from 'react';
 
 function SuggestCard({which_day, url, location_name, description, numDays  }){
+    const [isLoading, setIsLoading] = useState(false)
+
     return(
         <Element className="container_data" name={which_day} >
             <div className='container_day'>
@@ -9,8 +12,9 @@ function SuggestCard({which_day, url, location_name, description, numDays  }){
                 <h3 className='day'>{which_day} Day</h3>
                 { which_day+1 > numDays ? '' : <Link to={ (which_day+1) } smooth={true} duration={500} className='right'>⇢</Link>}
             </div>
-            <div className='container_img_data'> 
-                <img src={url} alt="park" className='img' />
+            <div className='container_img_data'>
+                {!isLoading && <img src='https://i.gifer.com/ZKZg.gif' alt='loading_gif' className='loading'/>}
+                <img src={url} alt={location_name} className='img' onLoad={() => setIsLoading(true)} />
                 <div className='data'>
                     <h3 className='title'>{location_name} </h3>
                     <p className='description'>{description} <a href="" className='clickable'>Read More...</a></p>
