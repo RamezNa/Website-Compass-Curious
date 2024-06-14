cae9633e0d5fef03fbad3a24845233b57ca4d65a
RamezNa <ramiznassar11@gmail.com>
2024-06-14 11:19:33 +0300

    build footer page

---
+++ FronEnd/src/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import './Footer.css'
+
+const Footer = () => {
+
+
+    return(
+        <footer>
+            <p className="photo">Photo credits: 
+                <a href="https://unsplash.com" target="_blank">Unsplash</a>, 
+                <a href="https://pexels.com" target="_blank">Pexels</a>, 
+                <a href="https://www.freepik.com/" target="_blank">freepik</a>
+            </p>
+            <p>&copy; 2024 Compass Curious. All rights reserved.</p>
+            <p >Designed and programmed by <span>Ramez Nassar</span></p>
+        </footer>
+        
+    )
+}
+
+export default Footer
