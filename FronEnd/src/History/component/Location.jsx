4323d34e67070cb47037bdf72bf3baacb403d543
RamezNa <ramiznassar11@gmail.com>
2024-06-14 11:26:45 +0300

    add loading

--- FronEnd/src/History/component/Location.jsx
+++ FronEnd/src/History/component/Location.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react"
+
 function Location( {url, nameLocation,numDays} ){
+
+    const [isLoading, setIsLoading] = useState(false)
 
     return(
         <div className="data">
-            <img className="img" src={url} alt={nameLocation}  />
+            {!isLoading && <img src='https://i.pinimg.com/originals/61/24/16/6124164e5582efe0c5d11fc85b263437.gif' alt='loading_gif' className='loading'/>}
+            <img className="img" src={url} alt={nameLocation} onLoad={() => setIsLoading(true)}  />
             <div className="aligment">
                 <h3 className="name_location">{nameLocation}</h3>
                 <p className="num_day">{numDays} {numDays > 1 ? 'Days' : 'Day'} </p>
             </div>
-            {/* <h3 className="name_location">{nameLocation}</h3>
-            <p className="num_day">{numDays} {numDays > 1 ? 'Days' : 'Day'} </p> */}
         </div>
     )
 }
