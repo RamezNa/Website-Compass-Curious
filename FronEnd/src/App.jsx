1ee3d97a13604c6ec18f01fa4d1274a42a397c61
RamezNa <ramiznassar11@gmail.com>
2024-06-14 11:17:18 +0300

    add pages

--- FronEnd/src/App.jsx
+++ FronEnd/src/App.jsx
@@ -4,7 +4,9 @@ import MainPage from "./MainPage/MainPage"
 import Login from "./Login/Login"
 import History from "./History/History"
 import Suggestion from "./Suggestion/Suggestion"
-
+import SignUp from "./Login/SignUp/SignUp"
+import Forget from "./Login/Forget/Forget"
+import Footer from "./Footer/Footer"
 
 function App(){
     return(
@@ -15,7 +17,10 @@ function App(){
                 <Route path="/Login" element={<Login />} />
                 <Route path="/History" element={<History />} />
                 <Route path="/Suggestion" element={<Suggestion />} />
+                <Route path="/SignUp" element={<SignUp />} />
+                <Route path="/Forget" element={<Forget />} />
             </Routes>
+            <Footer />
         </>
     )
 }
