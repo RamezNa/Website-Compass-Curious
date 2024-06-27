8a3517f48b42a097ce048045115d497694625ad2
RamezNa <ramiznassar11@gmail.com>
2024-06-27 09:36:08 +0300

    Add a suggestion bar, fix the trend display, and add the feature to move to the suggestion by clicking on the trend.

--- FronEnd/src/MainPage/MainPage.jsx
+++ FronEnd/src/MainPage/MainPage.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
 
 import './mainPage.css'
 
@@ -11,6 +13,9 @@ import { collection, query, orderBy, limit, getDocs } from 'firebase/firestore';
 
 
 function MainPage(){
+    const apiUrl = 'https://raw.githubusercontent.com/russ666/all-countries-and-cities-json/master/countries.json';
+
+    const server = ''//http://0.0.0.0:8000
 
     const sumbit_answer_w = 'https://cdn-icons-png.freepik.com/512/5486/5486234.png?ga=GA1.1.940078363.1718183127'
     const sumbit_answer_g = 'https://cdn-icons-png.freepik.com/512/5486/5486216.png?ga=GA1.1.940078363.1718183127'
@@ -21,12 +26,49 @@ function MainPage(){
 
     const maxDay = 30
     const minDay = 0
-
+  
     const [location, setlocation ] =  useState('')
-    const changeLocation = (event) =>{
-        {/* TODO check the location that fit the website that we need to search for */}
-        setlocation(event.target.value)
+    const [suggestionsCity, setSuggestionsCity] = useState([]);
+    const [suggestionsCountry, setSuggestionsCountry] = useState([]);
+    const [data_contries_cities, setData_contries_cities] = useState({})
+
+    const changeLocation = async (event) =>{
+        let loc_val = event.target.value
+        setlocation(loc_val)
+
+        if (loc_val.length > 2) {
+            
+            const found = [];
+            const foundCountry = []
+            const query = location.toLowerCase();
+            // move on all the json and check if there is one that containe what we search add it to the found
+            for (const country in data_contries_cities) {
+                // if it seem to search to country we but it in array
+                if (country.toLowerCase().startsWith(query)) {
+                    foundCountry.push({ country });
+                }
+                // search on the city and see if there is one seem to what i whant to find
+                for (const city of data_contries_cities[country]) {
+                    // if it seem to search to city we but it in array
+                    if (city.toLowerCase().startsWith(query)) {
+                        found.push({ country, city });
+                    }
+                }
+            }
+            setSuggestionsCity(found)
+            setSuggestionsCountry(foundCountry)
+
+        } else {
+            setSuggestionsCity([])
+            setSuggestionsCountry([])
+        }
     }
+
+    const handleSuggestionClick = (suggestion) => {
+        setlocation(suggestion)
+        setSuggestionsCity([])
+        setSuggestionsCountry([])
+      };        
 
     const [days, setdays ] =  useState(0)
     const changeDays = (event) =>{
@@ -47,8 +89,8 @@ function MainPage(){
         // TODO check that the location is valid :)
         event.preventDefault()
          // sent to server to update trend
-         try{
-            await fetch(`${server}/trend/${location}/${numdays}`)
+        try{
+            await fetch(`${server}/trend/${location}/${days}`)
         }catch (error) {
             console.error("Error fetching from server: ", error)
         }
@@ -58,12 +100,18 @@ function MainPage(){
     const [listOfTrends,getListOfTrends] = useState([])
     const [isLoading,setIsLoading] = useState(true)
 
+    const [focused , setFocused] = useState(false)
+
     useEffect( () =>{
         const  getData = async () =>{
             try {
-                const q = query(collection(db, '_trend'), orderBy('numTrend', 'desc'), limit(5));
-                const querySnapshot = await getDocs(q);
-                const trends = querySnapshot.docs.map(doc => doc.data());
+                // fetch the data of the countries and cities
+                const response = await axios.get(apiUrl)
+                setData_contries_cities(response.data)
+                // get the rend from the firestore
+                const q = query(collection(db, '_trend'), orderBy('numTrend', 'desc'), limit(5))
+                const querySnapshot = await getDocs(q)
+                const trends = querySnapshot.docs.map(doc => doc.data())
                 getListOfTrends(trends)
                 setIsLoading(false)
             } catch (error) {
@@ -86,13 +134,23 @@ function MainPage(){
                     <form onSubmit={handleSubmit}>
                         <div className="container_submit">
                             <img className='icon_search' src={icon_search} alt="search" />
-                            <input className='location_input'  type="text" value={location} onChange={changeLocation} placeholder='Where Do You Want To Travel' required/>
+                            <input className='location_input'  type="text" value={location} onChange={changeLocation} placeholder='Where Do You Want To Travel' onFocus={()=> setFocused(true)} onBlur={ ()=> setFocused(false) } required/>
                         </div>
+
+                        { ( suggestionsCity.length > 0 || suggestionsCountry.length > 0) && (
+                            <ul className={`container_list ${focused ? 'open' : 'close'}`} >
+                                {suggestionsCountry.map(({country},index) => (
+                                    <li key={index + 500} onClick={() => handleSuggestionClick(country)} className='suggest'>  -ˏˋ⋆ {country} ⋆ˊˎ- </li>
+                                ))}
+                                {suggestionsCity.map(({country, city},index) => (
+                                    <li key={index + 50} onClick={() => handleSuggestionClick(city)} className='suggest'>  -ˏˋ⋆ {city}, {country} ⋆ˊˎ- </li>
+                                ))}
+                            </ul>
+                        )}
                         
                         <div className="container_submit">
                             
                             <input className='day_input' type="number" value={days == 0 ? '' : days } max={maxDay} min={minDay} onChange={changeDays} placeholder='How Much Days' required/>
-                            {/* TODO move to page suggestion and make loading to the page  */}
                             <button type='submit' className='btn_submit'>
                                 <Img_Hover url_hovered={sumbit_answer_g} url_unHovered={sumbit_answer_w} class_name={'icon_go'} alt_name={"submit"} />
                             </button>
@@ -104,13 +162,12 @@ function MainPage(){
                 
                 <h1 className='title'>Inspired By Travelers</h1>
                 <h2 className='subTitle'>These Destinations Are Trending With Compass Curious Explorers!</h2>
-                {/* TODO Check witch one to choice for loading */}
+                
                 {isLoading && <img src='https://i.pinimg.com/originals/61/24/16/6124164e5582efe0c5d11fc85b263437.gif' alt='loading_gif' className='loading'/> }
-                {/* TODO make when click on the day it make a suggestion for me by the day that choice and the location */}
 
-                { listOfTrends.length == 0 ? <h3>-ˏˋ⋆ No one has searched yet. Be the first to discover trending destinations with Compass Curious Explorers! ⋆ˊˎ- </h3>:
+                { listOfTrends.length == 0 && !isLoading ? <h3>-ˏˋ⋆ No one has searched yet. Be the first to discover trending destinations with Compass Curious Explorers! ⋆ˊˎ- </h3>:
                 listOfTrends.map( (trend,index) => (
-                    <Box_Img_Description class_={ (index % 2 == 0 ) ? 'left' : 'right'} number={index + 1} number_of_days={trend.days}  name_location={trend.location}  description={trend.description} url={trend.url} key={index}/> 
+                    <Box_Img_Description class_={ (index % 2 == 0 ) ? 'left' : 'right'} number={index + 1} number_of_days={trend.days}  name_location={trend.location}  description={trend.description} url={trend.url} server={server}  key={index}/> 
                 ) )}
             </div>   
         </div>
