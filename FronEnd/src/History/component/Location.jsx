3be7b1af3c707b191a9fcd81d1bcfaec1762d743
RamezNa <ramiznassar11@gmail.com>
2024-07-06 12:17:20 +0300

    the click on the box Move to Save History for the same area and the advice that was received.

--- FronEnd/src/History/component/Location.jsx
+++ FronEnd/src/History/component/Location.jsx
@@ -1,14 +1,19 @@
 import { useState, useEffect } from "react"
+import { useNavigate } from 'react-router-dom'
 import { db } from "../../Firebase/firebase"
 import { where, query, getDocs, collection } from "firebase/firestore"
 
-function Location( {nameLocation,numDays} ){
+
+// TODO add field to the id of the data 
+function Location( {nameLocation, numDays, dataLocation} ){
+
+    const navigate = useNavigate()
 
     const [isLoading, setIsLoading] = useState(false)
     const [url, setUrl] = useState('')
 
-
     useEffect( () =>{
+        // get the img from the data base
         const start = async() =>{
             const q = query( collection(db , '_trend'), where('location', '==', nameLocation ) )
             const querySnapshot = await getDocs(q)
@@ -19,8 +24,13 @@ function Location( {nameLocation,numDays} ){
         start()
     }, [])
 
+    const handleMoveToSuggestion = () => {
+        navigate('/Suggestion', {state: {location: (nameLocation.trim().toLowerCase()), numdays: numDays, isHistory: true, data_: dataLocation }})
+        window.scrollTo(0, 0);
+    }
+
     return(
-        <div className="data">
+        <div className="data" onClick={handleMoveToSuggestion}>
             {!isLoading ? <img src='https://i.pinimg.com/originals/61/24/16/6124164e5582efe0c5d11fc85b263437.gif' alt='loading_gif' className='loading'/>:
             <img className="img" src={url} alt={nameLocation} />
             }
