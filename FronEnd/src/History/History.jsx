2410522c1526508e59271b3f85b1a420b2879a73
RamezNa <ramiznassar11@gmail.com>
2024-07-08 16:26:03 +0300

    incorporate the Gemini suggestion based on the user's preferences on page history

--- FronEnd/src/History/History.jsx
+++ FronEnd/src/History/History.jsx
@@ -1,21 +1,110 @@
 import './history.css'
 import Location from './component/Location'
+import LocationTrend from './component/LocationTrend'
 import { useEffect,useState } from 'react'
 import { auth,db } from '../Firebase/firebase'
 import { useNavigate } from 'react-router-dom'
-import { collection, query, getDocs,where } from 'firebase/firestore';
+import { collection, query, getDocs, where, deleteDoc, doc, updateDoc,arrayUnion } from 'firebase/firestore';
 
+import API_KEY from './component/giminiApi'
+import { GoogleGenerativeAI } from "@google/generative-ai"
 
 
 function History(){
+    const server = 'http://0.0.0.0:8000'
+
     const navigate = useNavigate()
 
     const [listOfHistory,setListOfHistory] = useState([])
     const [isLoading, setIsLoading] = useState(false)
 
+    // Gimini generate section
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const [suggestLocation, setSuggestLocation] = useState([])
+    const [isLoadingSug, setIsLoadingSug] = useState(true)
+
+    // generate a random code that containe number and digit
+    const generateRandomCode = (length) => {
+        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        return Array.from({ length }, () => characters[Math.floor(Math.random() * characters.length)]).join('');
+    };
+
+    // function that wait to specific time like wait
+    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));  
+
+    const generateText = async (prompt, querySnapshotUser) => {
+        // gimini code work to get the respone
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig:{"response_mime_type": "application/json"}})
+        const result = await model.generateContent(prompt)
+        const response = await result.response
+        // convert the string to list json
+        const res = JSON.parse(response.text())
+
+        // TODO check if the ',' in the res[index]['name]
+        // get the images to the location that i recived  
+        const list_location = res[0]['name'] + ',' + res[1]['name'] + ',' + res[2]['name'] + ',' + res[3]['name'] + ',' + res[4]['name']
+        const list_description = res[0]['description'] + '^' + res[1]['description'] + '^' + res[2]['description'] + '^' + res[3]['description'] + '^' + res[4]['description']
+        const codeGen = generateRandomCode(10)
+        try{
+
+            fetch(`${server}/suggestion_based_Love/${codeGen}/${list_location}`)///${list_description}
+
+            // wait and after that fetch the data from the firebase 
+            let time_wait = 28000
+            const q = query(collection(db, '_suggestImg'), where('codeGen', '==', codeGen))
+            
+            // fetch the data from the firebase
+            let querySnapshot = await getDocs(q)
+            while( querySnapshot.empty){
+                await delay(time_wait)
+                time_wait = time_wait / 2
+                querySnapshot = await getDocs(q)
+                console.log('i am waiting')
+            }
+            
+            let value_temp = querySnapshot.docs[0].data().res
+
+            value_temp = value_temp.map( ( temp , index ) => {
+                return{
+                    ...temp,
+                    description :res[index].description
+                }
+            } )
+
+            setSuggestLocation(value_temp)
+
+            //  remove it from the firebase 
+
+            const documentId = querySnapshot.docs[0].id
+            await deleteDoc(doc( db, '_suggestImg', documentId ))
+            console.log(value_temp)
+            // TODO save it in the field of the user
+            querySnapshotUser.forEach( async (doc) => {
+                const docRef = doc.ref;
+                // Update the document with the new value
+                try {
+                    // TODO check we need to do something else array
+                    await updateDoc(docRef, {
+                        isChanged: false, // Update isChanged field  
+                        basedLove: value_temp
+                    });
+                    setIsLoadingSug(false)
+                } catch (error) {
+                    console.error("Error updating document:", doc.id, error);
+                }
+
+            });
+
+        }catch(error){
+            console.error(error)
+        }
+
+    };
+
     useEffect( ()=>{
 
         const unsubscribe = auth.onAuthStateChanged( async () => {
+            
             try {
                 const email_user = auth?.currentUser?.email
 
@@ -26,9 +115,41 @@ function History(){
 
                 const q = query(collection(db, '_users'), where('email', '==', email_user))
                 const querySnapshot = await getDocs(q)
-                setListOfHistory(querySnapshot.docs[0].data().history)
+                const histor_data = querySnapshot.docs[0].data().history
+                setListOfHistory(histor_data)
                 setIsLoading(true)
 
+                // check if changed true and if the history empty or not and after that run the generate text base on what he want 
+                if( querySnapshot.docs[0].data().isChanged ){
+
+                
+                    if(histor_data.length > 0){
+
+                        // Using a for loop to concatenate array elements into a string
+                        let result = "";
+                        for (let i = 0; i < histor_data.length; i++) {
+                            result += histor_data[i]['location']
+                            if (i !== histor_data.lengt - 1) {
+                                result += ", " // Add a comma and space for separation 
+                            }
+                        }
+                        console.log('here the result')
+                        console.log(result)
+                        generateText('Could you recommend five cities or countries for me based on my previous searches? My previous searches for ' + result + '.' + 'provide to me the name and some description about city or country. return to me list of json response', querySnapshot)
+                        console.log('we are in the generatText with data')
+                    }else{
+
+                        generateText('Could you recommend five cities or countries for me based on populartie.' + 'provide to me the name and some description about city or country. return to me list of json response', querySnapshot)
+                        console.log('we are in the generatText without data :(')
+                    }
+                }else{
+                    // TODO fetch from the use the data 
+                    setSuggestLocation( querySnapshot.docs[0].data().basedLove)
+                    console.log('we are fetch the data from the firebase')
+                    setIsLoadingSug(false)
+                }
+                
+                
             } catch (error) {
                 console.error(error)
             }  
@@ -43,18 +164,37 @@ return(
             <div className='line'></div>
             <h1 className='title'>History</h1>
         </div>
+
         <h2 className='subTitle'>Continue planning your dream trip!<br/>Access your saved itineraries for inspiration.</h2>
         <div className='line'></div>
         {!isLoading && <img src='https://i.pinimg.com/originals/61/24/16/6124164e5582efe0c5d11fc85b263437.gif' alt='loading_gif' className='loading'/>}
         <div className="container_data">
-            {/* TODO make when clicked moved to page suggestion and load the suggestion */}
+            {/* when clicked moved to page suggestion and load the suggestion */}
             {   
                 listOfHistory.length == 0 && isLoading ? <h3 className='no_history'> -ˏˋ⋆ No locations saved yet. Explore new destinations that speak to your interests! ⋆ˊˎ- </h3> : 
                 listOfHistory.map( (loc,index) =>(
-                    <Location url={loc.url} nameLocation={loc.location} numDays={loc.days} dataLocation={loc.whatDoing} key={index} />
+                    <Location nameLocation={loc.location} numDays={loc.days} dataLocation={loc.whatDoing} key={index} />
                 ) )
             }
             
+        </div>
+        <div className='line'></div>
+        <div className="container_based_search">
+            <div className='header'>
+                <div className='line'></div>
+                <h1 className='title'>Based Search</h1>
+            </div>
+            <h2 className='subTitle'>Unsure where to start? We've got you covered. <br/>Discover your next favorite spot with Compass Curious.</h2>
+            <div className='line'></div>
+            <div className="container_data_sug">
+            {isLoadingSug && <img src='https://i.pinimg.com/originals/61/24/16/6124164e5582efe0c5d11fc85b263437.gif' alt='loading_gif' className='loading'/>}   
+            {   
+                suggestLocation.length == 0 && isLoadingSug ? <h3 className='no_history'> -ˏˋ⋆ we are fetch the data from the server ⋆ˊˎ- </h3> : 
+                suggestLocation.map( (loc,index) =>(
+                    <LocationTrend url={loc.url} nameLocation={loc.location_name} dataLocation={loc.description} key={index + 100} />
+                ) )
+            }
+            </div>
         </div>
     </div>
 )
